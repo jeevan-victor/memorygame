189f3232d3f632e18d43a0ed358d91bdb16d1c7f
jeevan-victor <163705520+jeevan-victor@users.noreply.github.com>
2024-04-01 13:59:19 +0800

    memory game

---
+++ project.js
@@ -0,0 +1,26 @@
+// document.getElementById("start-button").addEventListener("click", function() {
+//     document.getElementById("start-page").style.display = "none";
+//     document.getElementById("guessnumber-page").style.display = "block";
+//   });
+
+
+Generate random number
+  function generateNumber() {
+    let randomNum = Math.random() * 100000000;
+    randomNum = Math.floor(randomNum);
+    return randomNum;
+  }
+
+//  console.log(generateNumber());
+
+document.getElementById('start-button').addEventListener('click', function() {
+    // Generate a random number
+    let randomNumber = generateRandomNumber();
+  
+    // Display the random number on the game page
+    document.getElementById('random-number').textContent = "Random Number: " + randomNumber;
+  
+    // Hide the start page and show the game page
+    document.getElementById('start-page').style.display = 'none';
+    document.getElementById("guessnumber-page").style.display = 'block';
+  });
