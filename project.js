c962c785aa05a5823ae19e39a590d05edc2eab5e
jeevan-victor <163705520+jeevan-victor@users.noreply.github.com>
2024-04-02 17:01:49 +0800

    memory game

--- project.js
+++ project.js
@@ -1,23 +1,41 @@
 
+let countdownElement = document.getElementById("countdown");
+    let count = 5;
+    countdownElement.textContent = "Time remaining: " + count;
+
+
+
+
+document.getElementById("start-button").addEventListener("click", function() {
+    
+    let randomNumber = generateNumber();
+  
+
+    document.getElementById("random-number").textContent = randomNumber;
+  
+   
+    document.getElementById("start-page").style.display = "none";
+    document.getElementById("guessnumber-page").style.display = "block";
+   
+    let countdownInterval = setInterval(function() {
+      count--;
+      countdownElement.textContent = "Time remaining: " + count;
+      if (count === 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("guessnumber-page").style.display = "none";
+          document.getElementById("input-page").style.display = "block";
+      }
+    }, 1000);
+
+    
+  });
+
+
   function generateNumber() {
     let randomNum = Math.random() * 100000000;
     randomNum = Math.floor(randomNum);
     return randomNum;
   }
-
-
-
-document.getElementById('start-button').addEventListener('click', function() {
-    
-    let randomNumber = generateNumber();
-  
-
-    document.getElementById('random-number').textContent = randomNumber;
-  
-   
-    document.getElementById('start-page').style.display = 'none';
-    document.getElementById("guessnumber-page").style.display = 'block';
-  });
 
 
   
