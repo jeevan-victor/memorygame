32f8e0e76f8c2b42a4f12a214aa37c2d883264f2
jeevan-victor <163705520+jeevan-victor@users.noreply.github.com>
2024-04-02 21:28:59 +0800

    memory game

--- project.js
+++ project.js
@@ -3,20 +3,19 @@ let countdownElement = document.getElementById("countdown");
     let count = 5;
     countdownElement.textContent = "Time remaining: " + count;
 
+    let randomNumber = generateNumber();
+    let inputNumber;
 
-
-
+    //Start game page
 document.getElementById("start-button").addEventListener("click", function() {
     
-    let randomNumber = generateNumber();
-  
 
+    //Random Number page
     document.getElementById("random-number").textContent = randomNumber;
-  
-   
     document.getElementById("start-page").style.display = "none";
     document.getElementById("guessnumber-page").style.display = "block";
-   
+
+   //Countdown Timer
     let countdownInterval = setInterval(function() {
       count--;
       countdownElement.textContent = "Time remaining: " + count;
@@ -26,12 +25,22 @@ document.getElementById("start-button").addEventListener("click", function() {
           document.getElementById("input-page").style.display = "block";
       }
     }, 1000);
+});
 
-    
-  });
+//User input page
+document.getElementById("enter-button").addEventListener("click", function() {
+    inputNumber = document.getElementById("input-number").value;
+    inputNumber = parseInt(document.getElementById("input-number").value);
+    console.log(inputNumber);
 
+    if (inputNumber === randomNumber){
+        console.log("win");
+    } else console.log ("lose");
+}
 
-  function generateNumber() {
+);
+   
+    function generateNumber() {
     let randomNum = Math.random() * 100000000;
     randomNum = Math.floor(randomNum);
     return randomNum;
