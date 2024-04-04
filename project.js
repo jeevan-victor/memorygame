5ea1af764660ec7109a273c02840698172ccc67d
jeevan-victor <163705520+jeevan-victor@users.noreply.github.com>
2024-04-04 15:50:52 +0800

    Created 3 different difficulty levels with different random number functions

--- project.js
+++ project.js
@@ -2,7 +2,7 @@ let countdownElement = document.getElementById("countdown");
 let count = 5;
 countdownElement.textContent = "Time remaining: " + count;
 
-let randomNumber = generateNumber(8);
+let randomNumber = generateNumber();
 let inputNumber;
 let username;
 
@@ -13,9 +13,10 @@ username = document.getElementById("userName").value;
 console.log(username);
 }
 
-//Start game page
-document.getElementById("start-button").addEventListener("click", function() {
+//Start game page - easy
+document.getElementById("easy-button").addEventListener("click", function() {
 
+randomNumber = generateNumber(6);
 
 //Random Number page
 document.getElementById("random-number").textContent = randomNumber;
@@ -47,16 +48,17 @@ if (inputNumber === randomNumber){
     document.getElementById("correct-page").style.display = "block";
 
 //console.log("win");
-} else {document.getElementById("input-page").style.display = "none";
+} else {
+document.getElementById("input-page").style.display = "none";
 document.getElementById("wrong-page").style.display = "block";
 //console.log ("lose");
 }});
 
-// Correct number
+// Correct number - easy
 document.getElementById("continue").addEventListener("click", function() {
 
 // Increase the length of the random number
-randomNumber = generateNumber(randomNumber.toString(8).length + 1);
+randomNumber = generateNumber(randomNumber.toString().length + 1);
 
 // Display the new random number
 document.getElementById("random-number").textContent = randomNumber;
@@ -82,17 +84,92 @@ countdownInterval = setInterval(function() {
 });
 
 
-
-
-
-// Wrong number
+// Wrong number - easy
 document.getElementById("try-again").addEventListener("click", function() {
 
 
-  
-  
     // Otherwise, continue the game
-    randomNumber = generateNumber(randomNumber.toString(8).length);
+    randomNumber = generateNumber(randomNumber.toString().length);
+
+    // Display the new random number
+    document.getElementById("random-number").textContent = randomNumber;
+     document.getElementById("wrong-page").style.display = "none";
+    document.getElementById("guessnumber-page").style.display = "block";
+
+    count = 5;
+    countdownElement.textContent = "Time remaining: " + count;
+
+    countdownInterval = setInterval(function() {
+        count--;
+        countdownElement.textContent = "Time remaining: " + count;
+        if (count === 0) {
+            clearInterval(countdownInterval);
+            document.getElementById("guessnumber-page").style.display = "none";
+            document.getElementById("input-page").style.display = "block";
+        }
+    }, 1000);
+}
+);
+
+//Start game page - medium
+document.getElementById("medium-button").addEventListener("click", function() {
+
+randomNumber = generateNumber(8);
+
+//Random Number page
+document.getElementById("random-number").textContent = randomNumber;
+document.getElementById("start-page").style.display = "none";
+document.getElementById("guessnumber-page").style.display = "block";
+
+//Countdown Timer
+countdownInterval = setInterval(function() {
+  count--;
+  countdownElement.textContent = "Time remaining: " + count;
+  if (count === 0) {
+    clearInterval(countdownInterval);
+    document.getElementById("guessnumber-page").style.display = "none";
+      document.getElementById("input-page").style.display = "block";
+  }
+}, 1000);
+});
+
+
+// Correct number - medium
+document.getElementById("continue").addEventListener("click", function() {
+
+// Increase the length of the random number
+randomNumber = generateNumber(randomNumber.toString().length + 2);
+
+// Display the new random number
+document.getElementById("random-number").textContent = randomNumber;
+
+document.getElementById("correct-page").style.display = "none";
+document.getElementById("guessnumber-page").style.display = "block";
+
+
+count = 5;
+countdownElement.textContent = "Time remaining: " + count;
+
+
+countdownInterval = setInterval(function() {
+    count--;
+    countdownElement.textContent = "Time remaining: " + count;
+    if (count === 0) {
+      clearInterval(countdownInterval);
+      document.getElementById("guessnumber-page").style.display = "none";
+        document.getElementById("input-page").style.display = "block";
+    }
+  }, 1000);
+
+});
+
+
+// Wrong number - medium
+document.getElementById("try-again").addEventListener("click", function() {
+
+
+    // Otherwise, continue the game
+    randomNumber = generateNumber(randomNumber.toString().length);
 
     // Display the new random number
     document.getElementById("random-number").textContent = randomNumber;
@@ -116,25 +193,109 @@ document.getElementById("try-again").addEventListener("click", function() {
 );
 
 
+//Start game page - hard
+document.getElementById("hard-button").addEventListener("click", function() {
+
+randomNumber = generateNumber(10);
+
+//Random Number page
+document.getElementById("random-number").textContent = randomNumber;
+document.getElementById("start-page").style.display = "none";
+document.getElementById("guessnumber-page").style.display = "block";
+
+//Countdown Timer
+countdownInterval = setInterval(function() {
+  count--;
+  countdownElement.textContent = "Time remaining: " + count;
+  if (count === 0) {
+    clearInterval(countdownInterval);
+    document.getElementById("guessnumber-page").style.display = "none";
+      document.getElementById("input-page").style.display = "block";
+  }
+}, 1000);
+});
+
+// Correct number - hard
+document.getElementById("continue").addEventListener("click", function() {
+
+// Increase the length of the random number
+randomNumber = generateNumber(randomNumber.toString().length + 2);
+
+// Display the new random number
+document.getElementById("random-number").textContent = randomNumber;
+
+document.getElementById("correct-page").style.display = "none";
+document.getElementById("guessnumber-page").style.display = "block";
+
+
+count = 5;
+countdownElement.textContent = "Time remaining: " + count;
+
+
+countdownInterval = setInterval(function() {
+    count--;
+    countdownElement.textContent = "Time remaining: " + count;
+    if (count === 0) {
+      clearInterval(countdownInterval);
+      document.getElementById("guessnumber-page").style.display = "none";
+        document.getElementById("input-page").style.display = "block";
+    }
+  }, 1000);
+
+});
+
+
+// Wrong number - hard
+document.getElementById("try-again").addEventListener("click", function() {
+
+
+// Otherwise, continue the game
+randomNumber = generateNumber(randomNumber.toString().length);
+
+// Display the new random number
+document.getElementById("random-number").textContent = randomNumber;
+
+document.getElementById("wrong-page").style.display = "none";
+document.getElementById("guessnumber-page").style.display = "block";
+
+count = 5;
+countdownElement.textContent = "Time remaining: " + count;
+
+countdownInterval = setInterval(function() {
+    count--;
+    countdownElement.textContent = "Time remaining: " + count;
+    if (count === 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("guessnumber-page").style.display = "none";
+        document.getElementById("input-page").style.display = "block";
+    }
+}, 1000);
+}
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Function to generate a random number 
 function generateNumber(a) {
-let max = Math.pow(10, a) - 1;
-let randomNum = Math.floor(Math.random() * max);
-return randomNum;
+let min = Math.pow(10, a - 1); 
+let max = Math.pow(10, a) - 1; 
+return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// let highscore = 0;
-
-// if (inputNumber === randomNumber) {
-//     highscore++;
-// }
-
-
-// let wrongGuess = 0;
-
-// if (inputNumber === randomNumber) {
-
-//         document.getElementById("wrong-page").style.display = "none";
-//         document.getElementById("gameover-page").style.display = "block";
-// }
